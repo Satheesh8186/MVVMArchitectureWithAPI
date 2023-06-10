df46bf7a1c5e17829a805c1d01dd383892753c46
satheesh <sknaidu8186@gmail.com>
2023-06-10 15:55:19 -0400

    Initial Commit

---
+++ MVVMArchitectureWithAPI/MVVMArchitectureWithAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MVVMArchitectureWithAPIApp.swift
+//  MVVMArchitectureWithAPI
+//
+//  Created by satheesh kumar on 10/06/23.
+//
+
+import SwiftUI
+
+@main
+struct MVVMArchitectureWithAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
